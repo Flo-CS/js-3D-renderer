f276ed6b6ffbc491dddca96aac37e68846ab6c76
Flo-IT <53081078+Flo-IT@users.noreply.github.com>
2021-12-26 11:58:30 +0100

    Change the image color to the same as in the tutorial

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const image = new Image(WIDTH, HEIGHT);
 for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
         const color = new Color(
-            x / WIDTH * 256, y / HEIGHT * 256, 0
+            x / WIDTH * 256, (HEIGHT - y) / HEIGHT * 256, 256 / 2
         );
         image.putPixel(x, y, color);
 
